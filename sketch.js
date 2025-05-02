ef712151c0c74f22d128868efc2f323f87bd3837
TTOOBBYY5588 <ttoobbyy5588@gmail.com>
2025-05-02 11:48:05 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function draw() {
   }
 
   let x = (width - capture.width) / 2; // 計算影像的水平中心位置
-  let y = height - capture.height + 50; // 計算影像的垂直位置，並向下移動 50px
+  let y = height - capture.height + 50; // 計算影像的垂直位置，並向下移動 200px
 
   // 更新 graphics 的內容
   graphics.background(0); // 設定背景顏色為黑色
